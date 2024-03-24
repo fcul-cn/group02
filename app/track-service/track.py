add23d4d6ec98efb9bd8bc8b1788bb2c03893e10
André Santos <80883346+AndreSantos0@users.noreply.github.com>
2024-03-24 16:59:26 +0000

    Adds more funcionalities to the REST API.

--- app/track-service/track.py
+++ app/track-service/track.py
@@ -2,3 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.get("/api/tracks/<track_id>")
+def get_track(track_id):
+    
+@app.delete("/api/tracks/<track_id>")
+def delete_track(track_id):
+
+@app.post("/api/tracks")
+def post_track(track_id):
