0754f88abc7da2440ea078dce42d472f0e08e099
Magalhaes <jdiogo2302@gmail.com>
2024-03-30 12:12:18 +0000

    artist missing just microservice connections

--- app/artist-model-service/artist_model.py
+++ app/artist-model-service/artist_model.py
@@ -15,12 +15,13 @@ from grpc_interceptor.exceptions import NotFound, InvalidArgument, AlreadyExists
 
 def connect():
     try:
+        print("Connecting to the PostgreSQL database...")
         conn = psycopg2.connect(
             host=os.environ.get('POSTGRES_HOST'),
             user=os.environ.get('POSTGRES_USER'),
             password=os.environ.get('POSTGRES_PASSWORD'),
             port=os.environ.get('POSTGRES_PORT'),
-            database=os.environ.get('POSTGRES_DB_ARTIST')
+            database=os.environ.get('POSTGRES_DB')
         )
         return conn
     except (Exception, psycopg2.DatabaseError) as error:
@@ -30,7 +31,8 @@ class ArtistService(artist_pb2_grpc.ArtistService):
     def getArtist(self, request, context):
         try:
             artist_id = request.artist_id
-            if id <= 0:
+            print("artist_id: ", artist_id)
+            if artist_id <= 0:
                 raise InvalidArgument()
             conn = connect()
             cur = conn.cursor()
@@ -39,7 +41,7 @@ class ArtistService(artist_pb2_grpc.ArtistService):
             row = cur.fetchone()
             conn.commit()
             if not (row is None):
-                return GetArtistResponse(track=Artist(
+                return GetArtistResponse(artist=Artist(
                         artist_id=row[0],
                         artist_name=row[1],
                         artist_url=row[2],
@@ -57,9 +59,10 @@ class ArtistService(artist_pb2_grpc.ArtistService):
         check_if_name_exists = sql.SQL("SELECT 1 FROM Artist WHERE artist_name = %s")
         check_if_url_exists = sql.SQL("SELECT 1 FROM Artist WHERE artist_url = %s")
         query = sql.SQL("INSERT INTO Artist (artist_name, artist_url, artist_updated_at) VALUES (%s, %s, %s) RETURNING artist_id;")
+        get_new_artist = sql.SQL("SELECT * FROM Artist WHERE artist_id = %s;") 
         try:
-            artist_name = request.artist_name 
-            artist_url = request.artist_url
+            artist_name = request.artist.artist_name 
+            artist_url = request.artist.artist_url
             if not artist_name or not artist_url:
                 raise InvalidArgument()
             conn = connect()
@@ -72,10 +75,20 @@ class ArtistService(artist_pb2_grpc.ArtistService):
             if cur.fetchone():
                 raise AlreadyExists()
             # Add artist
-            cur.execute(query, (artist_name, artist_url, request.artist_updated_at))
+            cur.execute(query, (artist_name, artist_url, request.artist.artist_updated_at))
             artist_id = cur.fetchone()[0]
+            cur.execute(get_new_artist, (artist_id,))
+            row = cur.fetchone()
             conn.commit()
-            return AddArtistResponse(artist_id=artist_id)
+            if not (row is None):
+                return AddArtistResponse(artist=Artist(
+                        artist_id=row[0],
+                        artist_name=row[1],
+                        artist_url=row[2],
+                        artist_updated_at=str(row[3]),
+                    )
+                )
+            raise InvalidArgument()
         except (psycopg2.DatabaseError) as error:
             print(error)
         finally:
