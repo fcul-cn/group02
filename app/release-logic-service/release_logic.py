cbf5fec59669284c6143d7c08c04d3bedc53ce84
guilherme-dsantos <bolden_portadown.fpooe@slmails.com>
2024-04-01 09:08:01 +0000

    Implement release logic and model services

---
+++ app/release-logic-service/release_logic.py
@@ -0,0 +1,80 @@
+from flask import Flask, request
+import grpc
+import os
+from app_pb2 import GetReleaseRequest, DeleteReleaseRequest, AddReleaseRequest
+from app_pb2_grpc import ReleaseServiceStub, ReleaseServiceStub
+
+app = Flask(__name__)
+
+release_host = os.getenv("RELEASE_HOST", "localhost")
+release_channel = grpc.insecure_channel(f"{release_host}:50058")
+release_client = ReleaseServiceStub(release_channel)
+
+@app.get("/api/releases/<int:release_id>")
+def get_release(release_id):
+    try:
+        request = GetReleaseRequest(release_id=release_id)
+        response = release_client.GetRelease(request)
+        return {
+            "release_id": response.release.release_id,
+            "release_title": response.release.title,
+            "release_date": response.release.date,
+            "release_url": response.release.url,
+            "updated_on": response.release.updated_on,
+        }, 200
+    except grpc.RpcError as rpc_error:
+        if rpc_error.code() == grpc.StatusCode.NOT_FOUND:
+            return rpc_error.details(), 404
+        if rpc_error.code() == grpc.StatusCode.INVALID_ARGUMENT:
+            return rpc_error.details(), 400
+    except Exception as e:
+        return "Internal error: " + str(e), 500
+    
+@app.delete("/api/releases/<int:release_id>")
+def delete_release(release_id):
+    try:
+        request = DeleteReleaseRequest(release_id=release_id)
+        response = release_client.DeleteRelease(request)
+        return {
+            "release_id": response.release.release_id,
+            "release_title": response.release.title,
+            "release_date": response.release.date,
+            "release_url": response.release.url,
+            "updated_on": response.release.updated_on,
+        }, 200
+    except grpc.RpcError as rpc_error:
+        if rpc_error.code() == grpc.StatusCode.NOT_FOUND:
+            return rpc_error.details(), 404
+        if rpc_error.code() == grpc.StatusCode.INVALID_ARGUMENT:
+            return rpc_error.details(), 400
+    except Exception as e:
+        return "Internal error: " + str(e), 500
+
+@app.post("/api/releases")
+def post_releases():
+    try:
+        request_body = request.json
+        add_request = AddReleaseRequest(release=NewRelease(
+            title=str(request_body['title']),
+            date=str(request_body['date']),
+            url=str(request_body['url']),
+            updated_on=int(request_body['updated_on']),
+        ))
+        response = track_client.addTrack(add_request)
+        return {
+            "release_id": response.release.release_id,
+            "release_title": response.release.title,
+            "release_date": response.release.date,
+            "release_url": response.release.url,
+            "updated_on": response.release.updated_on,
+        }, 201   
+    except grpc.RpcError as rpc_error:
+        if rpc_error.code() == grpc.StatusCode.NOT_FOUND:
+            return rpc_error.details(), 404
+        if rpc_error.code() == grpc.StatusCode.INVALID_ARGUMENT:
+            return rpc_error.details(), 400
+        if rpc_error.code() == grpc.StatusCode.ALREADY_EXISTS:
+            return rpc_error.details(), 403
+    except Exception as e:
+        return "Internal error: " + str(e), 500
+
---
+++ app/release-model-service/release_model.py
@@ -0,0 +1,130 @@
+import psycopg2
+from psycopg2 import sql
+from concurrent import futures
+import grpc
+import os
+from grpc_interceptor import ExceptionToStatusInterceptor
+from app_pb2 import Track, GetReleaseResponse, DeleteReleaseResponse, AddReleaseResponse
+import app_pb2_grpc
+
+def connect():
+    try:
+        conn = psycopg2.connect(
+            host=os.environ.get('POSTGRES_HOST'),
+            user=os.environ.get('POSTGRES_USER'),
+            password=os.environ.get('POSTGRES_PASSWORD'),
+            port=os.environ.get('POSTGRES_PORT'),
+            database=os.environ.get('POSTGRES_DB')
+        )
+        return conn
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+class ReleaseService(app_pb2_grpc.ReleaseServiceServicer):
+    def GetRelease(self, request, context):
+        try:
+            if request.release_id <= 0:
+                context.set_code(grpc.StatusCode.INVALID_ARGUMENT)
+                context.set_details("Release's id must be higher than 0.")
+                context.abort()
+            conn = connect()
+            cur = conn.cursor()
+            query = sql.SQL("SELECT * FROM Release WHERE release_id = %s;") 
+            cur.execute(query, (request.release_id,))
+            row = cur.fetchone()
+            conn.commit()
+            if (row is None):
+                context.set_code(grpc.StatusCode.NOT_FOUND)
+                context.set_details("Release not found.")
+                context.abort()
+            return GetReleaseResponse(release=Release(
+                    release_id=row[0],
+                    title=row[1],
+                    date=row[2],
+                    url=row[3],
+                    updated_on=row[4],
+                ))
+        except (psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            if conn is not None:
+                conn.close()
+         
+    def DeleteRelease(self, request, context):
+        try:
+            if request.release_id <= 0:
+                context.set_code(grpc.StatusCode.INVALID_ARGUMENT)
+                context.set_details("Release's id must be higher than 0.")
+                context.abort()
+            conn = connect()
+            cur = conn.cursor()
+            query = sql.SQL("SELECT * FROM Release WHERE release_id = %s;") 
+            cur.execute(query, (request.track_id,))
+            row = cur.fetchone()
+            if (row is None):
+                context.set_code(grpc.StatusCode.NOT_FOUND)
+                context.set_details("Release not found.")
+                context.abort()
+            response = DeleteReleaseResponse(release=Release(
+                    release_id=row[0],
+                    title=row[1],
+                    date=row[2],
+                    url=row[3],
+                    updated_on=row[4],
+                )
+            )
+            query = sql.SQL("DELETE FROM Release WHERE release_id = %s;") 
+            cur.execute(query, (request.release_id,))
+            conn.commit()
+            return response
+        except (psycopg2.DatabaseError) as error:
+            print(error)
+            conn.rollback()
+        finally:
+            if conn is not None:
+                conn.close()
+
+    def AddRelease(self, request, context):
+        try:
+            conn = connect()
+            cur = conn.cursor()
+            query = sql.SQL("INSERT INTO Release (title, date, url, updated_on) VALUES (%s,%s,%s,CURRENT_DATE) RETURNING genre_id;") 
+            cur.execute(query, (request.release.title, request.release.date, request.release.url))
+            release_id = cur.fetchone()[0]
+            query = sql.SQL("SELECT * FROM Release WHERE release_id = %s;") 
+            cur.execute(query, (release_id,))
+            row = cur.fetchone()
+            conn.commit()
+            if not (row is None):
+                return AddReleaseResponse(release=
+                    Release(
+                        release_id=row[0],
+                        title=row[1],
+                        date=row[2],
+                        url=row[3],
+                        updated_on=row[4],
+                    )
+                )
+            raise InvalidArgument()
+        except (psycopg2.DatabaseError) as error:
+            print(error)
+            conn.rollback()
+        finally:
+            if conn is not None:
+                conn.close()
+
+def serve():
+    interceptors = [ExceptionToStatusInterceptor()]
+    server = grpc.server(
+        futures.ThreadPoolExecutor(max_workers=10), interceptors=interceptors
+    )
+    app_pb2_grpc.add_ReleaseServiceServicer_to_server(
+        ReleaseService(), server
+    )
+    server.add_insecure_port("[::]:50058")
+    server.start()
+    server.wait_for_termination()
+
+
+if __name__ == "__main__":
+    serve()
