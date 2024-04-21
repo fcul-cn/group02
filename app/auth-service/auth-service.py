3fd9376986d319bf673a1900b899e293ca4210b1
Lavian13 <lcviana136@gmail.com>
2024-04-21 11:30:05 +0100

    state implemented. not sure about path in callback

--- app/auth-service/auth-service.py
+++ app/auth-service/auth-service.py
@@ -13,15 +13,21 @@ app = Flask(__name__)
 #    load_dotenv(ENV_FILE)
 load_dotenv("auth.env")
 
+
 secret_key = secrets.token_bytes(32)  # Generate a 32-byte (256-bit) secret key
 app.secret_key = secret_key
-
+state_request = dict()
 # auth-url endpoint
 @app.route('/api/auth/token/validate', methods=['POST','GET', 'PUT', 'DELETE'])
 def validate_token():
+    #nonce = secrets.token_urlsafe()
+    #state_request[nonce]=request
+
     token = request.headers.get('Authorization')
     auth0_domain = os.getenv('AUTH0_DOMAIN')
     json_header = {'content-type': 'application/json'}
+
+
     #print(request.method)
     #print(request.path)
     #print(request.headers)
@@ -29,17 +35,6 @@ def validate_token():
     #print(request.data)
     #print(dict(request.headers))
     #print(dict(request.args))
-    if request.data:
-        json=request.json
-    else: 
-        json=''
-    session['original_request'] = {
-        'method': request.method,
-        'path': request.path,
-        'headers': dict(request.headers),
-        'query_params': dict(request.args),
-        'data': json,  # If content-type is application/json
-    }
     
     if request.method=="GET" and '/api/playlists' not in request.path:
         return '', 200
@@ -52,6 +47,7 @@ def validate_token():
     else:
         return '', 401
 
+
 # auth-signin endpoint
 @app.route('/api/auth/login', methods=['GET', 'POST'])
 def login():
@@ -60,133 +56,105 @@ def login():
     client_id = os.getenv('AUTH0_CLIENT_ID')
     redirect_uri = os.getenv('REDIRECT_URI')
 
-    return redirect(f"https://{auth0_domain}/authorize?audience=https://{auth0_domain}/api/v2/&response_type=token&client_id={client_id}&redirect_uri={redirect_uri}")
+    nonce = secrets.token_urlsafe()
+    state_request[nonce]=request
+
+    return redirect(f"https://{auth0_domain}/authorize?audience=https://{auth0_domain}/api/v2/&response_type=token&client_id={client_id}&redirect_uri={redirect_uri}&state={nonce}")
     
 
 @app.route('/callback')
 def callback():
-    # Extract token from URL fragment
-    original_request = session.get('original_request')
-    if original_request:
-        path = original_request.get('path')
-        method = original_request.get('method')
-        headers = original_request.get('headers')
-        query_params = original_request.get('query_params')
-        data = original_request.get('data')
-        if '/api/tracks' in path:
-            service = 'track-logic-service'
-        elif '/api/artists' in path :
-            service = 'artist-logic-service'
-        elif '/api/genres' in path :
-            service = 'genre-logic-service'
-        elif '/api/playlists' in path :
-            service = 'playlist-logic-service'
-        elif '/api/releases' in path :
-            service = 'release-logic-service'
-        else:
-            service = None
-
-        url_fragment = request.full_path.split('#')[1]
+    user_id=''
+    raw_url = request.environ.get('REQUEST_URI')
+    if raw_url is None:
+        raw_url = request.environ.get('REQUEST_URI_RAW')
+    print("Raw URL:", raw_url)
+    print("url", request.url)
+    print("full", request.full_path)
+    #print(request.environ.get('RAW_URI'))
+        
+    url_fragment = request.full_path.split('#')[1]
 
         # Parse the URL fragment to get key-value pairs
-        params = {}
-        for param in url_fragment.split('&'):
-            key, value = param.split('=')
-            params[key] = value
-
+    params = {}
+    for param in url_fragment.split('&'):
+        key, value = param.split('=')
+        params[key] = value
         # Retrieve the access token
-        token = params.get('access_token')
-        try:
-            jwks_url = 'https://dev-whwklff83w2gqupr.us.auth0.com/.well-known/jwks.json'
-            jwks_response = requests.get(jwks_url)
-            jwks = jwks_response.json()
+    token = params.get('access_token')
+    nonce = params.get('state')
 
-            # Extract the RSA public key from the JWKS
-            rsa_public_key = None
-            for key in jwks['keys']:
-                if key['alg'] == 'RS256':
-                    rsa_public_key = jwt.algorithms.RSAAlgorithm.from_jwk(key)
-                    break
-            if rsa_public_key:
+    oldrequest = state_request[nonce]
+
+    try:
+        jwks_url = 'https://dev-whwklff83w2gqupr.us.auth0.com/.well-known/jwks.json'
+        jwks_response = requests.get(jwks_url)
+        jwks = jwks_response.json()
+
+        # Extract the RSA public key from the JWKS
+        rsa_public_key = None
+        for key in jwks['keys']:
+            if key['alg'] == 'RS256':
+                rsa_public_key = jwt.algorithms.RSAAlgorithm.from_jwk(key)
+                break
+        if rsa_public_key:
             # Decode the JWT token
-                decoded_token = jwt.decode(token, rsa_public_key, algorithms=["RS256"], audience='https://dev-whwklff83w2gqupr.us.auth0.com/api/v2/')
+            decoded_token = jwt.decode(token, rsa_public_key, algorithms=["RS256"], audience='https://dev-whwklff83w2gqupr.us.auth0.com/api/v2/')
                 # Extract user ID from decoded token
-                user_id = decoded_token.get('sub')
+            user_id = decoded_token.get('sub')
                 # You can store or use the user ID as needed
-                print("User",user_id.split("|")[1])
-        except jwt.ExpiredSignatureError:
-            return 'Token expired', 401  # Token expired
-        except jwt.InvalidTokenError as e:
-            print("Invalid token:", e)
-            print(jwt.__version__)
-            return 'Invalid token', 401  # Invalid token
+            print("User",user_id.split("|")[1])
+    except jwt.ExpiredSignatureError:
+        return 'Token expired', 401  # Token expired
+    except jwt.InvalidTokenError as e:
+        print("Invalid token:", e)
+        print(jwt.__version__)
+        return 'Invalid token', 401  # Invalid token
 
-        '''token = request.args.get('access_token') #wrong
+    if oldrequest.data:
+        json=oldrequest.json
+    else: 
+        json=''
+    path = oldrequest.path
+    method = oldrequest.method
+    headers = dict(oldrequest.headers)
+    query_params = dict(oldrequest.args)
+    data = json
+    data['user_id'] = user_id
+    if '/api/tracks' in path:
+        service = 'track-logic-service'
+    elif '/api/artists' in path :
+        service = 'artist-logic-service'
+    elif '/api/genres' in path :
+        service = 'genre-logic-service'
+    elif '/api/playlists' in path :
+        service = 'playlist-logic-service'
+    elif '/api/releases' in path :
+        service = 'release-logic-service'
+    else:
+         service = None
 
-        print(token);
-        if token:
-            try:
-                # Decode the JWT token
-                decoded_token = jwt.decode(token, algorithms=["RS256"], audience=os.getenv('AUTH0_CLIENT_ID'))
-                # Extract user ID from decoded token
-                user_id = decoded_token.get('sub')
-                # You can store or use the user ID as needed
-                session['user_id'] = user_id
-
-            except jwt.ExpiredSignatureError:
-                return 'Token expired', 401  # Token expired
-            except jwt.InvalidTokenError:
-                return 'Invalid token', 401  # Invalid token
-            
-            print(user_id)
-
-            if method != "GET":
-                # Add user_id to data for non-GET requests
-                if data is None:
-                    data = {}
-                data['user_id'] = user_id'''
-
-        try:
-            response = requests.request(
-                method=method,
-                url=f'http://{service}{path}',
-                headers=headers,
-                params=query_params,
-                json=data
-            )
+    try:
+        response = requests.request(
+            method=method,
+            url=f'http://{service}{path}',
+            headers=headers,
+            params=query_params,
+            json=data
+        )
 
                 # Send the POST request to the /api/tracks endpoint in the track-logic service
                 #response = requests.post('http://track-logic-service/api/tracks', headers=headers, json=data)
 
-            if response.status_code == 200:
+        if response.status_code == 200:
                 # Extract data from the response
-                data = response.json()
+            data = response.json()
                 # Include the data in the response to the client
-                return jsonify(data), 200
-            else:
-                return 'Request to {path} failed'
-        except Exception as e:
-            return 'Internal error: ' + str(e), 500
-        #else:
-        #    return 'Failed to log in', 401
-    else:
-        # Handle case where original request details are not found in session
-        return 'Error: Original request details not found in session', 401
-    
-    
-'''    
-#for sending new request with token to ingress(doesnt work bcs client doesnt get the response)
-headers = {
-        'Host': 'group02.cn.com',
-        'Authorization': f'Bearer {token}',
-    }
-
-    # Send the new request
-    response = requests.request(
-        method=original_request['method'],
-        url=f"https://group02.cn.com{original_request['path']}",
-        headers=headers,
-    )'''
+            return jsonify(data), 200
+        else:
+            return 'Request to {path} failed'
+    except Exception as e:
+        return 'Internal error: ' + str(e), 500
     
 @app.route('/health', methods=['GET'])
 def health_check():
