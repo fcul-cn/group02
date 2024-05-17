f1c1be7b8bdb85070d7d8099fa1f340630116e52
guilherme-dsantos <bolden_portadown.fpooe@slmails.com>
2024-05-17 16:57:25 +0000

    update: Migrate Istio 1.21.2 -> 1.22.0. Various fixes

--- deploy_cluster.sh
+++ deploy_cluster.sh
@@ -11,20 +11,12 @@ kubectl create secret generic json-key --from-literal "API_TOKEN=$(cat JSON-KEY.
 kubectl create secret docker-registry artifact-registry --docker-server=https://europe-west4-docker.pkg.dev/ --docker-email=artifact-registry@fcul-cn-ads.iam.gserviceaccount.com --docker-username=_json_key --docker-password="$(cat ARTIFACT-REGISTRY.json)"
 
 #Istio
-curl -L https://istio.io/downloadIstio | sh -
+curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.22.0 TARGET_ARCH=x86_64 sh -
 export PATH="$PATH:/home/$USER/group02/istio-1.21.2/bin"
 yes | istioctl install
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout tls.key -out tls.crt -subj "/CN=*"
 kubectl create -n istio-system secret tls istio-ingressgateway-certs --key tls.key --cert tls.crt
-kubectl apply -n istio-system -f - <<EOF
-apiVersion: security.istio.io/v1beta1
-kind: PeerAuthentication
-metadata:
-  name: default
-spec:
-  mtls:
-    mode: STRICT
-EOF
+kubectl create secret generic grafana-admin-credentials --from-env-file=grafana-admin-credentials.env -n istio-system
 kubectl label namespace default istio-injection=enabled prometheus-monitoring=enabled
 
 #Deploy
